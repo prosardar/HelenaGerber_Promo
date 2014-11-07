d60bd1f396e287c980431cd3a10e1e5b551e9b1f
Sardar <prosardar@ya.ru>
2014-11-07 22:39:56 +0500

    первый коммит

---
+++ HelenaGerber_Promo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelenaGerber_Promo.Startup))]
+namespace HelenaGerber_Promo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
