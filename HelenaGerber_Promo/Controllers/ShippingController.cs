60f53448ecc5166385b45a8391915e649e39fe15
Sardar <prosardar@ya.ru>
2014-11-08 00:18:43 +0500

    футер типа внизу

---
+++ HelenaGerber_Promo/Controllers/ShippingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HelenaGerber_Promo.Controllers
+{
+    public class ShippingController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
